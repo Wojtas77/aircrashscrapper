cf75fdbe93ee75fce1b0658230cbb83271573389
Zwirzu <30357555+Zwirzu@users.noreply.github.com>
2017-08-07 00:58:28 +0200

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,4 @@
-import urlib2
+import urllib2
 from bs4 import BeautifulSoup
 import csv
 baseurl = 'http://www.planecrashinfo.com'
@@ -6,3 +6,8 @@ dburl = 'http://www.planecrashinfo.com/database.htm'
 page = urllib2.urlopen(dburl)
 soup = BeautifulSoup(page)
 linki = soup.findAll("a")
+links = linki.get('href')
+for lin in [:-1]:
+    url = baseurl + lin
+    so = BeautifulSoup(urllib2.urlopen(url)
+    
